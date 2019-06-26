9925f876200b9e88486a7084a1ad1f93c71c0215
libertadcc <libertadchapinalcervantes@gmail.com>
2019-06-26 14:14:31 +0200

    Add exercises with arrays

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -22,21 +22,78 @@ btnClick.addEventListener('click', add);
 btnReset.addEventListener('click', reset);
 
 //<---------- Timer ---------->
-
+//                AYUDA
+const time = document.querySelector('.time');
 const btnStart = document.querySelector('.btn__start');
 const btnStop = document.querySelector('.btn__stop');
-const timer = document.querySelector('.timer');
 
-let second = setInterval(startTimer, 1000);
-let ss = 0;
-function startTimer () {
 
-  ss = ss + 1;
-  timer.innerHTML = ss;
+let timer = 0;
+let temp;
+const incrementAndShowTimer = () => {
+  timer++;
+  time.innerHTML = timer;
+
+};
+temp = setInterval(incrementAndShowTimer, 1000);
+
+const stopTimer = () => {
+  clearInterval(temp)
 }
-
-function stopTimer () {
-  clearInterval(ss);
-}
-btnStart.addEventListener('click', startTimer);
+btnStart.addEventListener('click', incrementAndShowTimer);
 btnStop.addEventListener('click', stopTimer);
+//<---------- Arrays ---------->
+
+// examples with .MAP
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const inflatedMarks = numbers.map(item => item + 1);
+console.log('inflatedMarks', inflatedMarks);
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const helloNames = names.map(name => 'Hola ' + name);
+console.log('helloNames', helloNames);
+
+// examples with .FILTER
+const longNames = names.filter(name => name.length >5);
+console.log('longNames', longNames);
+
+const users = [
+  { name: 'María', isPremium: false },
+  { name: 'Lucía', isPremium: true },
+  { name: 'Susana', isPremium: true },
+  { name: 'Rocío', isPremium: false },
+  { name: 'Inmaculada', isPremium: false },
+];
+const premium = users.filter(user => user.isPremium === true);
+console.log('premium', premium);
+
+const pins = [2389, 2384, 2837, 5232, 8998];
+const evenPins = pins.filter(pin => pin%2 === 0);
+console.log('evenPins', evenPins);
+
+const evenNumbers = numbers.filter(number => number%2 === 0);
+console.log('evenNumbers', evenNumbers);
+
+const usuarias = [
+  { name: 'María', isPremium: false, pin: 2389 },
+  { name: 'Lucía', isPremium: true, pin: 2384 },
+  { name: 'Susana', isPremium: true, pin: 2837 },
+  { name: 'Rocío', isPremium: false, pin: 5232 },
+  { name: 'Inmaculada', isPremium: false, pin: 8998 },
+];
+const usuariasTop = usuarias.filter(user =>  user.pin%2===0);
+console.log('usuariasTop', usuariasTop);
+
+//examples with .REDUCE
+const scores = [4, 2, 7, 8, 6, 7, 9, 1, 2, 6, 7];
+const result = scores.reduce((acc, number) => acc + number, 0);
+console.log('result', result);
+
+const resultNumber = numbers.reduce((acc, number) => acc + number, 0);
+console.log('resultNumber', resultNumber);
+
+const times = [56, 9, 45, 28, 35];
+const half = times.reduce((acc, number) => acc + number, 0)/times.length;
+console.log('half', half);
+
+
